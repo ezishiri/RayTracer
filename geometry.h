b4353e1a88e5f3f1702a2b6ce754116cef6bceb7
Edwin Zishiri <40304716+ezishiri@users.noreply.github.com>
2023-05-18 22:59:39 -0400

    ray_intersect

--- geometry.h
+++ geometry.h
@@ -6,7 +6,6 @@
 #include <vector>
 using namespace std;
 
-
 // for future improvements I could probably use a template for all the diff
 // types of vectors it's possible to work with (combinations of floats, doubles,
 // ints, etc)
@@ -24,28 +23,22 @@ struct Vec3f {
     z = c;
   }
 
-
-  float operator[](size_t index)
-{
+  float operator[](size_t index) {
     if (index >= 3) {
-        cout << "Array index out of bound, exiting";
-        exit(0);
+      cout << "Array index out of bound, exiting";
+      exit(0);
     }
-   if (index == 0){
-    return x; 
-  }
-  else if (index == 1){
-    return y; 
-  }
-  else if (index == 2){
-    return z; 
-  }
-  else {
+    if (index == 0) {
+      return x;
+    } else if (index == 1) {
+      return y;
+    } else if (index == 2) {
+      return z;
+    } else {
       std::cout << " Index out of bounds, exiting";
       exit(0);
+    }
   }
-    
-}
 
   // operator overloading for printing out values of 3d float vectors
   friend std::ostream &operator<<(std::ostream &os, const Vec3f &v) {
@@ -99,6 +92,11 @@ struct Vec3f {
 
     return Vec3f(i, j, k);
   }
+
+  Vec3f normalize() {
+    float magnitude = sqrt(x * x + y * y + z * z);
+    return Vec3f(x / magnitude, y / magnitude, z / magnitude);
+  }
 };
 
 // now repeat for 2d float vectors
@@ -117,6 +115,29 @@ struct Vec2f {
   friend std::ostream &operator<<(std::ostream &os, const Vec2f &v) {
     os << "(" << v.x << ", " << v.y << ")";
     return os;
+  }
+};
+
+// TODO: implement sphere
+struct Sphere {
+  Vec3f center;
+  float radius;
+
+  Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+  bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
+    Vec3f L = center - orig;
+    float tca = L.dot(dir);
+    float d2 = L.dot(L) - tca * tca;
+    if (d2 > radius * radius)
+      return false;
+    float thc = sqrtf(radius * radius - d2);
+    t0 = tca - thc;
+    float t1 = tca + thc;
+    if (t0 < 0)
+      t0 = t1;
+    if (t0 < 0)
+      return false;
+    return true;
   }
 };
 
