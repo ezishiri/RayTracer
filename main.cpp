bbfd5b22d5544061ab9cff314e55e4e7294b70e4
Edwin Zishiri <40304716+ezishiri@users.noreply.github.com>
2023-05-24 23:12:20 -0400

    draw spheres, add lighting

--- main.cpp
+++ main.cpp
@@ -1,21 +1,50 @@
 #include "geometry.h"
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <limits>
 #include <vector>
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const Sphere &sphere) {
-  float sphere_dist = std::numeric_limits<float>::max();
-  if (!sphere.ray_intersect(orig, dir, sphere_dist)) {
-    return Vec3f(0.2, 0.7, 0.8); // color 1
+bool scene_intersect(const Vec3f &orig, const Vec3f &dir,
+                     const std::vector<Sphere> &spheres, Vec3f &hit, Vec3f &N,
+                     Material &material) {
+  float spheres_dist = std::numeric_limits<float>::max();
+  for (size_t i = 0; i < spheres.size(); i++) {
+    float dist_i;
+    if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist) {
+      spheres_dist = dist_i;
+      hit = orig + dir * dist_i;
+      N = (hit - spheres[i].center).normalize();
+      material = spheres[i].material;
+    }
   }
-  return Vec3f(0.4, 0.4, 0.3); // color 2
+  return spheres_dist < 1000;
 }
 
-void render(const Sphere &sphere) {
-  const int width = 1024;
-  const int height = 768;
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir,
+               const std::vector<Sphere> &spheres,
+               const std::vector<Light> &lights) {
+  Vec3f point, N;
+  Material material;
+
+  if (!scene_intersect(orig, dir, spheres, point, N, material)) {
+    return Vec3f(0.2, 0.3, 0.2); // background color
+  }
+  float diffuse_light_intensity = 0;
+  for (size_t i = 0; i < lights.size(); i++) {
+    Vec3f light_dir = (lights[i].position - point).normalize();
+    diffuse_light_intensity +=
+        lights[i].intensity * std::max(0.f, light_dir.dot(N));
+  }
+  return material.diffuse_color * diffuse_light_intensity;
+}
+
+void render(const std::vector<Sphere> &spheres,
+            const std::vector<Light> &lights) {
+  const int width = 2048;
+  const int height = 1560;
   const float fov = M_PI / 2.;
   std::vector<Vec3f> framebuffer(width *
                                  height); // array of 3d vectors (rgb vals)
@@ -27,7 +56,8 @@ void render(const Sphere &sphere) {
                 (float)height;
       float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
       Vec3f dir = Vec3f(x, y, -1).normalize();
-      framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, sphere);
+      framebuffer[i + j * width] =
+          cast_ray(Vec3f(0, 0, 0), dir, spheres, lights);
     }
   }
 
@@ -46,16 +76,40 @@ void render(const Sphere &sphere) {
 }
 
 int main() {
-  Sphere sphere1(Vec3f(-3, 0, -16), 2);
-  // Sphere sphere2(Vec3f(3, 0, 16), 2);
-  // Sphere sphere3(Vec3f(-10, 8, 4), 5);
-  // Sphere sphere4(Vec3f(4, 5, 6), 8);
-  // Sphere sphere5(Vec3f(11, -10, 3), 2);
-  render(sphere1);
-  // render(sphere2);
-  // render(sphere3);
-  // render(sphere4);
-  // render(sphere5);
+  srand(time(NULL));
 
+  std::vector<Sphere> spheres;
+  // generate twenty spheres
+  const int num_spheres = 20; // number of spheres to add
+  const float spacing = 3.0;  // spacing between spheres
+
+  for (int i = 0; i < num_spheres; i++) {
+    float x = rand() % 20 - 10; // random x coordinate between -10 and 10
+    float y = rand() % 20 - 10; // random y coordinate between -10 and 10
+    float z = rand() % 10 - 20; // random z coordinate between -20 and -10
+    float x_color = (float)rand() / (float)RAND_MAX;
+    float y_color = (float)rand() / (float)RAND_MAX;
+    float z_color = (float)rand() / (float)RAND_MAX;
+    x_color = (float)((int)(x_color * 100)) /
+              100; // limit precision to 2 decimal places
+    y_color = (float)((int)(y_color * 100)) / 100;
+    z_color = (float)((int)(z_color * 100)) / 100;
+    float radius = 2;
+    Material color(Vec3f(x_color, y_color, z_color));
+    spheres.push_back(Sphere(Vec3f(x, y, z), radius, color));
+  }
+
+  std::vector<Light> lights;
+  lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
+
+  render(spheres, lights);
+  int message[] = {119, 104, 97,  116, 32,  100, 111, 32,  121, 111, 117,
+                   32,  116, 104, 105, 110, 107, 32,  97,  98,  111, 117,
+                   116, 32,  109, 121, 32,  116, 105, 110, 121, 32,  114,
+                   101, 110, 100, 101, 114, 101, 114, 33,  32};
+
+  for (int i = 0; i < sizeof(message) / sizeof(message[0]); i++) {
+    cout << (char)message[i];
+  }
   return 0;
 }
